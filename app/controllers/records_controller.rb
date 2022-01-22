f4f321d14310619a9ac8151ef4ce52c1105df25b
Michael Roxas <michaelroxas.od@gmail.com>
2022-01-22 08:38:12 +0800

    Create controller for records
    Had some difficulty here as the form doesn't necessarily show when a patient's record is rendered.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,20 @@
 class RecordsController < ApplicationController
+    def create
+        @patient = Patient.find(params[:patient_id])
+        @record = @patient.records.create(record_params)
+        redirect_to article_path(@patient)
+    end
+    
+    private
+    def record_params
+        params.require(:record).permit(right_eye_sph:string 
+                                            right_eye_cyl:string 
+                                            right_eye_axis:string 
+                                            left_eye_sph:string 
+                                            
+                                            left_eye_cyl:string 
+                                            left_eye_axis:string 
+                                            
+                                            both_eyes_add:string)
+    end
 end
